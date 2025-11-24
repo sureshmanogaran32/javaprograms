ed33d6dc11ffa9ab5a45d1077cfd90be9ce06f77
suresh <sureshmanogaran32@gmail.com>
2025-11-24 17:03:54 +0530

    Swapping Integers 2 and 3 digits

---
+++ javaprograms/src/test/java/com/demo/javaprograms/Program01_Swapping_Two_Integer.java
@@ -0,0 +1,27 @@
+package com.demo.javaprograms;
+
+import java.util.Scanner;
+
+public class Program01_Swapping_Two_Integer {
+
+	public static void main(String[] args) {
+		int a =10; int b=20;
+		int temp;
+		System.out.println("Before Swapping A = "+a);
+		System.out.println("Before Swapping B = "+b);
+	//Approach 1
+		/*
+		temp = a;
+		a=b;
+		b=temp;
+		*/
+	//Approach 2
+		a=a+b; //10+20 =30
+		b=a-b; //30-20 = 10
+		a=a-b; //30-10 =20
+		
+		System.out.println("After Swapping A = "+a);
+		System.out.println("After Swapping B = "+b);
+	}
+
+}
