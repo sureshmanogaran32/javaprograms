b0683a72a3c327cef3c66885a757e92853d8281e
suresh <sureshmanogaran32@gmail.com>
2025-11-26 00:00:33 +0530

    Reverse String 3 Approach

---
+++ javaprograms/src/test/java/com/demo/javaprograms/Program03_ReverseString.java
@@ -0,0 +1,28 @@
+package com.demo.javaprograms;
+
+public class Program03_ReverseString {
+	public static void main(String[] args) {
+		String s = "ABCD";
+		System.out.println("Before Reverse String: "+s);
+		System.out.println("Length of String: "+s.length());
+		String rev="";
+		//Approach 1 by using charAt()
+	/*	for(int i=s.length()-1; i>=0;i--) {
+			rev=rev+s.charAt(i);
+			System.out.println("Reverse String: "+rev);
+		}  
+	*/
+		//Approach 2 by using string builder
+	/*
+		StringBuilder sb =new StringBuilder(s);
+		rev = sb.reverse().toString();
+		System.out.println("Reverse String: "+rev);
+	*/
+		//Approach 1 by using toCharArray()
+		char[] charArray = s.toCharArray();
+		for(char c:charArray) {
+			rev=c+rev;
+			System.out.println("Reverse String: "+rev);
+		}
+	}
+}
