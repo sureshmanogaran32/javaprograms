aacf787e077005bd9804642b42a5eb6b4d1b2bc6
suresh <sureshmanogaran32@gmail.com>
2025-11-26 16:18:02 +0530

    Odd and Even Number

---
+++ javaprograms/src/test/java/com/demo/javaprograms/Program05_Odd_Even_Number.java
@@ -0,0 +1,14 @@
+package com.demo.javaprograms;
+
+public class Program05_Odd_Even_Number {
+
+	public static void main(String[] args) {
+		int n=5;
+			if(n%2==0) {
+				System.out.println("Given Number "+n +" is: Even Number");
+			}else
+			{
+				System.out.println("Given Number " +n +" is: Odd Number");
+			}
+	}
+}
