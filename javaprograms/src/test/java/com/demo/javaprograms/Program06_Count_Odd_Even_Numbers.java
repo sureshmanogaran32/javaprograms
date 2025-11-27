5a03301ea94ad8d19e1955882cf5265f3a978152
suresh <sureshmanogaran32@gmail.com>
2025-11-28 00:28:34 +0530

    Count Odd & Even Numbers

---
+++ javaprograms/src/test/java/com/demo/javaprograms/Program06_Count_Odd_Even_Numbers.java
@@ -0,0 +1,38 @@
+package com.demo.javaprograms;
+
+public class Program06_Count_Odd_Even_Numbers {
+
+	public static void main(String[] args) {
+
+		int number =12345;
+		int n =10;
+		int evenCount=0;
+		int oddCount=0;
+		//Type 1  count the each digit from the given number
+		/*
+		while(number!=0) {
+			int lastDigit = number%10;
+			if(lastDigit%2==0) {
+				evenCount++;
+			}else {
+				oddCount++;
+			}
+			int newNumber =number/10;
+			number=newNumber;
+		}
+		
+	*/
+		//Type 2 count the numbers untill the given number
+			for(int i=1;i<=n;i++) {
+				if(i%2==0) {
+					evenCount++;
+				}else {
+					oddCount++;
+				}
+			}
+
+		System.out.println("Even Numbers Count From The Input : "+evenCount);
+		System.out.println("Odd Numbers Count From The Input : "+oddCount);
+	}
+
+}
